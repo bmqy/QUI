238fc4bf536554f009fd55503550aa539d4f1b77
bmqy <qq88268459@126.com>
2020-04-02 11:57:05 +0800

    backup

--- src/views/Text.vue
+++ src/views/Text.vue
@@ -2,11 +2,10 @@
   <div class="container-full">
     <div class="container w1200">
       <h1>文本演示</h1>
-      <div class="margin-top">
+      <div class="margin-top bg-gray">
         <p
           v-for="(item, index) in colors"
           :key="index"
-          class="text-sm"
           :class="`text-${item.name}`"
         >
           我是一段{{ item.title }}的文字
@@ -19,6 +18,7 @@
         <p class="text-blue text-sm">这是演示文字</p>
         <p class="text-orange text-sm">这是演示文字</p>
         <p class="bg-orange text-sm">这是演示文字</p>
+        <p class="bg-gray text-sm">这是演示文字</p>
       </div>
       <div class="margin-top">
         <p class="bg-white text-xs">这是演示文字</p>
