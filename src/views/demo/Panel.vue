3e3eeab1354a165da1695dc47c41a9d808afa0c6
北门清燕 <qq88268459@126.com>
2020-06-03 17:38:38 +0800

    update panel example

--- src/views/demo/Panel.vue
+++ src/views/demo/Panel.vue
@@ -3,9 +3,70 @@
     <div class="container w1200">
       <h1>面板演示</h1>
       <div class="panel border">
-        <div class="panel-head">
+        <div class="panel-head bg-gray">
           <div class="title">注册会员</div>
           <a href="#" class="more">更多</a>
+        </div>
+        <div class="panel-body">
+          <ul class="list list-style-disc text-ellipsis">
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容1
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容2
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容3
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容4
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容5
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容6
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容7
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容8
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容9
+            </li>
+            <li class="">
+              列表内容列表内容列表内容列表内容列表内容列表内容列表内容列表内容10
+            </li>
+          </ul>
+        </div>
+        <div class="panel-foot text-center">
+          <a href="#" class="btn line-orange">查看更多</a>
+        </div>
+      </div>
+
+      <div class="panel border margin-top">
+        <div class="panel-head clearfix bg-gray">
+          <div class="pull-left">
+            <div id="nav" class="nav">
+              <a href="/" class="router-link-active">首页</a>
+              <a href="/text" class="">文本</a>
+              <a href="/button" class="">按钮</a>
+              <a href="/list" class="">列表</a>
+              <a href="/grid" class="">宫格</a>
+              <a href="/form" class="">表单</a>
+              <a
+                href="/panel"
+                class="router-link-exact-active router-link-active"
+                >面板</a
+              >
+              <a href="/docs" class="">文档</a>
+            </div>
+          </div>
+          <div class="pull-right">
+            <a href="#" class="more">更多</a>
+          </div>
         </div>
         <div class="panel-body">
           <ul class="list list-style-disc text-ellipsis">
