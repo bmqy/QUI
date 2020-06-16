e05ca78179bc1b6a1de13cf09c0490569bb80a39
bmqy <qq88268459@126.com>
2020-06-16 11:34:27 +0800

    update col -> row

--- src/router/index.js
+++ src/router/index.js
@@ -16,56 +16,47 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import("../views/demo/Text.vue")
+    component: () => import("../views/demo/Text.vue")
   },
   {
     path: "/button",
     name: "按钮",
-    component: () =>
-      import("../views/demo/Button.vue")
+    component: () => import("../views/demo/Button.vue")
   },
   {
-    path: "/col",
+    path: "/row",
     name: "栅格",
-    component: () =>
-      import("../views/demo/Col.vue")
+    component: () => import("../views/demo/Row.vue")
   },
   {
     path: "/list",
     name: "列表",
-    component: () =>
-      import("../views/demo/List.vue")
+    component: () => import("../views/demo/List.vue")
   },
   {
     path: "/grid",
     name: "宫格",
-    component: () =>
-      import("../views/demo/Grid.vue")
+    component: () => import("../views/demo/Grid.vue")
   },
   {
     path: "/form",
     name: "表单",
-    component: () =>
-      import("../views/demo/Form.vue")
+    component: () => import("../views/demo/Form.vue")
   },
   {
     path: "/table",
     name: "表格",
-    component: () =>
-      import("../views/demo/Table.vue")
+    component: () => import("../views/demo/Table.vue")
   },
   {
     path: "/panel",
     name: "面板",
-    component: () =>
-      import("../views/demo/Panel.vue")
+    component: () => import("../views/demo/Panel.vue")
   },
   {
     path: "/tab",
     name: "tab",
-    component: () =>
-      import("../views/demo/Tab.vue")
+    component: () => import("../views/demo/Tab.vue")
   }
 ];
 
