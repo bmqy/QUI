8191923f789777709f843117716b4847cf482d0a
bmqy <qq88268459@126.com>
2020-06-22 14:56:07 +0800

    add col-offset, col-push,col-pull

--- src/views/demo/Row.vue
+++ src/views/demo/Row.vue
@@ -5,21 +5,32 @@
       <div class="row border">
         <div v-for="(item, index) in 12" :key="index" :class="[`col-1`]">
           <div class="bg-gray padding-mini">
-            {{ item }}
+            {{ `col-1` }}
           </div>
         </div>
       </div>
       <div class="row row-gap margin-top">
         <div v-for="(item, index) in 12" :key="index" :class="[`col-1`]">
           <div class="bg-gray">
-            {{ item }}
+            {{ `col-1` }}
           </div>
         </div>
       </div>
       <div class="row row-gap">
         <div v-for="(item, index) in 12" :key="index" :class="[`col-${item}`]">
           <div class="bg-gray">
-            {{ item }}
+            {{ `col-${item}` }}
+          </div>
+        </div>
+      </div>
+      <div class="row row-gap margin-top">
+        <div
+          v-for="(item, index) in 12"
+          :key="index"
+          :class="[`col-1`, index != 0 ? `col-offset-${index}` : '']"
+        >
+          <div class="bg-gray">
+            {{ index != 0 ? `col-1 col-offset-${item}` : "col-1" }}
           </div>
         </div>
       </div>
