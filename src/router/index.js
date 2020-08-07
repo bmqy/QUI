89be80de804bafa17bbd7098413e5a80b0f5167d
bmqy <qq88268459@126.com>
2020-08-07 15:59:14 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: Home
   },
   {
-    path: "/QUI/?page=text",
+    path: "/QUI/text",
     name: "文本",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
@@ -19,49 +19,49 @@ const routes = [
     component: () => import("../views/demo/Text.vue")
   },
   {
-    path: "/QUI/?page=button",
+    path: "/QUI/button",
     name: "按钮",
     component: () => import("../views/demo/Button.vue")
   },
   {
-    path: "/QUI/?page=row",
+    path: "/QUI/row",
     name: "栅格",
     component: () => import("../views/demo/Row.vue")
   },
   {
-    path: "/QUI/?page=list",
+    path: "/QUI/list",
     name: "列表",
     component: () => import("../views/demo/List.vue")
   },
   {
-    path: "/QUI/?page=grid",
+    path: "/QUI/grid",
     name: "宫格",
     component: () => import("../views/demo/Grid.vue")
   },
   {
-    path: "/QUI/?page=form",
+    path: "/QUI/form",
     name: "表单",
     component: () => import("../views/demo/Form.vue")
   },
   {
-    path: "/QUI/?page=table",
+    path: "/QUI/table",
     name: "表格",
     component: () => import("../views/demo/Table.vue")
   },
   {
-    path: "/QUI/?page=panel",
+    path: "/QUI/panel",
     name: "面板",
     component: () => import("../views/demo/Panel.vue")
   },
   {
-    path: "/QUI/?page=tab",
+    path: "/QUI/tab",
     name: "tab",
     component: () => import("../views/demo/Tab.vue")
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes
 });
